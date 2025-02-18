448458afe4f61ea6f0b935d1666233e267e44fb2
AravindKumar40m <aravindkumar40m@gmail.com>
2025-02-19 01:29:15 +0530

    change the menubar and make responsive for intro page

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -16,13 +16,14 @@ const MenuBar = () => {
   const toggleSubMenu = (index) => {
     setSubMenuActive((prev) => ({ ...prev, [index]: !prev[index] }));
   };
+
   return (
-    <div
-      className={`fixed top-5 right-5 z-50 ${
-        menuActive ? "hamburger-guide" : ""
-      }`}
-    >
-      <div className="cursor-pointer w-8 h-8" onClick={toggleMenu}>
+    <div className="fixed top-5 right-5 z-50">
+      {/* Menu Icon */}
+      <div
+        className="cursor-pointer w-8 h-8 z-50 relative md:w-10 md:h-10"
+        onClick={toggleMenu}
+      >
         <span
           className={`block h-1 bg-white transition-transform ${
             menuActive ? "translate-y-2.5 rotate-45" : ""
@@ -39,6 +40,8 @@ const MenuBar = () => {
           }`}
         />
       </div>
+
+      {/* Menu Overlay */}
       <div
         className={`fixed inset-0 bg-gray-800 bg-opacity-90 flex items-center justify-center transition-transform ${
           menuActive ? "scale-100 z-40" : "scale-0"
@@ -46,25 +49,25 @@ const MenuBar = () => {
         onClick={toggleMenu}
       >
         <ul
-          className="text-center text-white text-xl space-y-4"
+          className="text-center text-white text-xl space-y-6 p-4 w-4/5 sm:w-3/5 md:w-2/5"
           onClick={(e) => e.stopPropagation()}
         >
-          <li>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
             <Link href="/events">Events</Link>
           </li>
-          <li>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
             <Link href="/schedule">Schedule</Link>
           </li>
-          <li>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
             <Link href="/workshops">Workshops</Link>
           </li>
-          <li>
-            <Link href="/workshops">Sponsor</Link>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
+            <Link href="/sponsor">Sponsor</Link>
           </li>
-          <li>
+          <li className="hover:text-yellow-300 text-lg md:text-xl">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>

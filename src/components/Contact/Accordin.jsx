4707930c4729e9c0ca45e77758a7fa30e5384777
rajeshgit2004@gmail.com <rajeshgit2004@gmail.com>
2025-02-26 03:16:35 +0530

    Added Validation

--- src/components/Contact/Accordin.jsx
+++ src/components/Contact/Accordin.jsx
@@ -1,64 +1,84 @@
-import React, { useState } from "react";
-import { FaChevronDown } from "react-icons/fa"; // Import down arrow icon
+import React, { useState } from 'react'
+import { FaChevronDown } from 'react-icons/fa' // Import down arrow icon
 
 const Accordion = () => {
-  const [openIndex, setOpenIndex] = useState(null);
+  const [openIndex, setOpenIndex] = useState(null)
 
   const toggleAccordion = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
-  };
+    setOpenIndex(openIndex === index ? null : index)
+  }
 
   const accordionData = [
     {
-      question: "General Inquiries  :",
+      question: 'General Inquiries:',
       answers: [
         <>
-          Gokul Ramana <span className="accordinside1">9342194955</span>
+          Gokul Ramana{' '}
+          <a href='tel:+919342194955' className='accordinside1'>
+            +91 9342194955
+          </a>
         </>,
         <>
-          Swetha <span className="accordinside1">9360831815</span>
+          Swetha{' '}
+          <a href='tel:+919360831815' className='accordinside1'>
+            +91 9360831815
+          </a>
         </>,
         <>
-          <span className="accordinside3">mathrix2025@gmail.com</span>
+          <a href='mailto:mathrix2025@gmail.com' className='accordinside3'>
+            mathrix2025@gmail.com
+          </a>
         </>,
       ],
     },
     {
-      question: "Sponsor Inquiries",
+      question: 'Sponsor Inquiries',
       answers: [
         <>
-          Harshidha <span className="accordinside2">9080436437</span>
+          Harshidha{' '}
+          <a href='tel:+919080436437' className='accordinside2'>
+            9080436437
+          </a>
         </>,
         <>
-          Metta Surendhar <span className="accordinside2">8122801247</span>
+          Metta Surendhar{' '}
+          <a href='tel:+918122801247' className='accordinside2'>
+            8122801247
+          </a>
         </>,
         <>
-          Logeshwari<span className="accordinside2">8778158668</span>
+          Logeshwari{' '}
+          <a href='tel:+918778158668' className='accordinside2'>
+            8778158668
+          </a>
         </>,
         <>
-          <span className="accordinside3">
+          <a
+            href='mailto:mathrix.industrialrelations@gmail.com'
+            className='accordinside3'
+          >
             mathrix.industrialrelations@gmail.com
-          </span>
+          </a>
         </>,
       ],
     },
-  ];
+  ]
 
   return (
-    <div className="accordion">
+    <div className='accordion'>
       {accordionData.map((item, index) => (
-        <div key={index} className="accordion-item">
+        <div key={index} className='accordion-item'>
           <div
-            className="accordion-title"
+            className='accordion-title'
             onClick={() => toggleAccordion(index)}
           >
             <h3>{item.question}</h3>
             <FaChevronDown
-              className={`icon ${openIndex === index ? "rotate" : ""}`}
+              className={`icon ${openIndex === index ? 'rotate' : ''}`}
             />
           </div>
           <div
-            className={`accordion-content ${openIndex === index ? "open" : ""}`}
+            className={`accordion-content ${openIndex === index ? 'open' : ''}`}
           >
             {item.answers.map((answer, ansIndex) => (
               <p key={ansIndex}>{answer}</p>
@@ -67,7 +87,7 @@ const Accordion = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Accordion;
+export default Accordion

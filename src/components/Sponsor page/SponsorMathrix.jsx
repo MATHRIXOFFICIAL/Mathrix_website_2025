d2a03735cb66bc872f0fa62b04298b61b5e7c2ac
rajeshgit2004@gmail.com <rajeshgit2004@gmail.com>
2025-03-19 16:22:08 +0530

    Added Photo

--- src/components/Sponsor page/SponsorMathrix.jsx
+++ src/components/Sponsor page/SponsorMathrix.jsx
@@ -22,13 +22,13 @@ const SponsorMathrix = () => {
       name: 'Viral',
     },
     {
-      image: '/SponsorImage/magnipower.jpg',
+      image: '/SponsorImage/m.png',
       sponsortype: 'Technology and Beverage Sponsor',
       name: 'Viral',
     },
     {
-      image: '/SponsorImage/images.png',
-      sponsortype: 'Printing Sponsor',
+      image: '/SponsorImage/i.png',
+      sponsortype: 'Prize Partner',
       name: 'Viral',
     },
   ]
@@ -36,10 +36,10 @@ const SponsorMathrix = () => {
   return (
     <div className='min-h-screen p-4'>
       <div className='flex flex-col gap-5 justify-center items-center py-4'>
-        <p className='text-4xl font-extrabold text-red-500 text-center'>
-          Our Sponsors
+        <p className='text-4xl font-extrabold text-red-500 text-center hammersmith'>
+          {/* Our Sponsors */}
         </p>
-        <p className='text-neutral-600 text-xl text-center'>
+        <p className='text-white text-3xl text-center mt-0 hammersmith'>
           We are proud to present our incredible lineup of sponsors!
         </p>
 
@@ -47,9 +47,13 @@ const SponsorMathrix = () => {
         <div className='gridcontainer'>
           {Sponsors.map((item, key) => (
             <div key={key} className='book'>
-                <p className='sponsortitle'>{item.sponsortype}</p>
+              <p className='sponsortitle dmsans'>{item.sponsortype}</p>
               <div className='cover'>
-              <img src={item.image} alt={item.name} className='sponsor-image' />
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className='sponsor-image'
+                />
               </div>
             </div>
           ))}

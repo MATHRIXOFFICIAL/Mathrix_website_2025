51b344738ab9936692672c9746a94ca5abda3d98
Viswanathan P <drummerviswa@gmail.com>
2025-03-25 00:00:57 +0530

    Fix: Styles🎨

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -13,15 +13,15 @@ export default function about() {
       <MenuBar />
 
       <div className="flex flex-col text-white text-center md:text-left mt-10 md:mt-20 mb-10 hammersmith">
-        <h2 className="text-4xl text-center md:text-6xl font-extrabold mb-5">
-          Mathrix&apos;25
-        </h2>
-        <h1 className="text-4xl md:text-6xl font-extrabold text-teal-500 text-center hammersmith">
+        <h2 className="text-4xl text-center md:text-6xl text-teal-500 font-extrabold mb-5">
           Our Sponsors
+        </h2>
+        <h1 className="text-4xl md:text-6xl font-extrabold text-center hammersmith">
+          Mathrix&apos;25
         </h1>
       </div>
 
-      <p className="text-white text-3xl text-center hammersmith">
+      <p className="text-white text-xl underline decoration-wavy decoration-secondary-500 decoration-1 text-center hammersmith">
         We are proud to present our incredible lineup of sponsors!
       </p>
       <div className="min-h-screen w-full flex items-center justify-center dmsans">
